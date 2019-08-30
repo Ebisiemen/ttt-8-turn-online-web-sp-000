a6a4d3a5c311fc80f26e4925c363db3fae466423
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-08-30 17:22:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,7 @@
 def  display_board(board) 
   print " #{board[0]} | #{board[1]} | #{board[2]} "
   print "-----------"
-  print " #{board[3]} | #{board[4]} | #{board[5} "
+  print " #{board[3]} | #{board[4]} | #{board[5]} "
   print "-----------"
   print " #{board[6]} | #{board[7]} | #{board[8]} "
 end   
